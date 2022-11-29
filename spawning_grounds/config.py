877f5539d7e3d7ef8dd733061d7aafbda8d5fdcc
ruksi <me@ruk.si>
2022-11-29 15:29:23 +0200

    🛠️ Add spawning grounds for the fish
    Generates test files with the given configuration

---
+++ spawning_grounds/config.py
@@ -0,0 +1,22 @@
+import dataclasses
+import json
+from pathlib import Path
+
+
+@dataclasses.dataclass
+class Config:
+    username: str
+    access_key_id: str
+    secret_access_key: str
+    region: str
+    bucket: str
+    datasets: dict
+
+
+def load_config() -> Config:
+    here = Path(__file__).parent
+    config_path = Path(here, 'config.json')
+    with config_path.open() as fp:
+        values = json.load(fp)
+    config = Config(**values)
+    return config
