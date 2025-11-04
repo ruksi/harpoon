b763a51c0b661d78eb5adb324002b18e9a7614f6
Ruksi Korpisara <me@ruk.si>
2025-11-04 11:13:19 +0200

    Add skeleton for Genesis

---
+++ genesis/__main__.py
@@ -0,0 +1,27 @@
+import logging
+import argparse
+
+log = logging.getLogger(__file__)
+
+
+def main(*, dataset_name: str, file_count: int) -> None:
+    log.info(
+        f"TODO: functionality to generate a dataset version under '{dataset_name}' with {file_count} files"
+    )
+
+
+def cli() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset-name", type=str, default="verses")
+    parser.add_argument("--file-count", type=int, default=50)
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+    try:
+        main(**args.__dict__)
+    except Exception as e:
+        log.exception(e)
+
+
+if __name__ == "__main__":
+    cli()
