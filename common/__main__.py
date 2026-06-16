2027831e5ce4f404169c004f883937c24e601489
Ruksi Korpisara <me@ruk.si>
2026-06-16 12:40:12 +0300

    Add step for preset testing

---
+++ common/__main__.py
@@ -0,0 +1,24 @@
+import logging
+import sys
+import time
+
+log = logging.getLogger(__file__)
+
+
+def main() -> None:
+    duration = 1.0
+    log.info(f"I'll take a quick nap! ({duration}s)")
+    time.sleep(duration)
+
+
+def cli() -> None:
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    log.info(f"Called with parameters: {sys.argv[1:]}")
+    try:
+        main()
+    except Exception as e:
+        log.exception(e)
+
+
+if __name__ == "__main__":
+    cli()
